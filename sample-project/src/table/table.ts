9f6d336706110f3b178f0a89299a6647b5483c84
Utkarsh Jain <xutkarshjain@gmail.com>
2025-07-12 04:07:39 +0530

    add table search with debouncing

--- sample-project/src/table/table.ts
+++ sample-project/src/table/table.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { debounceTime, Subject, BehaviorSubject, takeUntil } from 'rxjs';
 
 interface ColumnData {
   name: string;
@@ -15,17 +17,32 @@ interface userData {
 
 @Component({
   selector: 'app-table',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './table.html',
   styleUrl: './table.css',
 })
-export class Table {
+export class Table implements OnInit, OnDestroy {
+  searchText = '';
+  filteredTableData: userData[] = [];
+  searchSubject = new BehaviorSubject<string>('');
+  private destroy$ = new Subject<void>();
+
   tableData: userData[] = [
     { id: 1, name: 'Alice', age: 25 },
     { id: 2, name: 'Bob', age: 30 },
     { id: 3, name: 'Charlie', age: 22 },
     { id: 4, name: 'Diana', age: 28 },
     { id: 5, name: 'Ethan', age: 35 },
+    { id: 6, name: 'Alice', age: 31 },
+    { id: 7, name: 'Bob', age: 26 },
+    { id: 8, name: 'Frank', age: 29 },
+    { id: 9, name: 'Grace', age: 24 },
+    { id: 10, name: 'Alice', age: 27 },
+    { id: 11, name: 'Helen', age: 33 },
+    { id: 12, name: 'Charlie', age: 21 },
+    { id: 13, name: 'Ian', age: 32 },
+    { id: 14, name: 'Diana', age: 30 },
+    { id: 15, name: 'Bob', age: 28 },
   ];
 
   tableColumns: ColumnData[] = [
@@ -35,10 +52,36 @@ export class Table {
     { name: 'Action', value: 'action' },
   ];
 
+  ngOnInit() {
+    this.filterTable();
+    this.searchSubject
+      .pipe(debounceTime(500), takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.filterTable();
+      });
+  }
+
   deleteDataRow(id: any) {
     const indToDelete = this.tableData.findIndex((row) => row.id == id);
-    console.log('indToDelete', indToDelete);
-
     this.tableData.splice(indToDelete, 1);
+    this.filterTable();
+  }
+
+  onSearch() {
+    setTimeout(() => {
+      this.searchSubject.next(this.searchText);
+    }, 0);
+  }
+
+  filterTable() {
+    this.filteredTableData = this.tableData.filter((row: userData) => {
+      return row.name
+        .toLowerCase()
+        .includes(this.searchText.trim().toLowerCase());
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.complete();
   }
 }
